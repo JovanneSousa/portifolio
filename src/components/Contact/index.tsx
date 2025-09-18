453d8db44749a44f628f9fceaa5597d2b982177c
Jovanne Sousa <jovanemaciel943@gmail.com>
2025-09-18 13:48:54 -0400

    feat: finalizando seção de contato, e iniciando seção de formulário de contato

---
+++ src/components/Contact/index.tsx
@@ -0,0 +1,98 @@
+import { ContactSection } from './styles'
+
+const Contact = () => {
+  return (
+    <ContactSection>
+      <div className="container">
+        <div className="title">
+          <h2>
+            Entre em <span>Contato</span>
+          </h2>
+          <p>Vamos conversar sobre seu próximo projeto</p>
+        </div>
+        <div className="content">
+          <div className="info">
+            <h3>Vamos trabalhar juntos!</h3>
+            <p className="text">
+              {' '}
+              Estou sempre aberto a discutir novas oportunidades, projetos
+              interessantes ou simplesmente trocar ideias sobre tecnologia.
+              Entre em contato!
+            </p>
+            <div className="info-rede">
+              <div className="icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <rect width="20" height="16" x="2" y="4" rx="2" />
+                  <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
+                </svg>
+              </div>
+              <div className="info-contact">
+                <p className="title-contact">Email</p>
+                <p className="address">jovanemaciel943@gmail.com</p>
+              </div>
+            </div>
+            <div className="info-rede">
+              <div className="icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
+                </svg>
+              </div>
+              <div className="info-contact">
+                <p className="title-contact">Email</p>
+                <p className="address">jovanemaciel943@gmail.com</p>
+              </div>
+            </div>
+            <div className="info-rede">
+              <div className="icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0" />
+                  <circle cx="12" cy="10" r="3" />
+                </svg>
+              </div>
+              <div className="info-contact">
+                <p className="title-contact">Email</p>
+                <p className="address">jovanemaciel943@gmail.com</p>
+              </div>
+            </div>
+          </div>
+          <form action="submit">
+            <label htmlFor="name">Nome</label>
+            <input type="text" id="name" placeholder="Seu nome completo" />
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" placeholder="seu@email.com" />
+            <label htmlFor="message">Mensagem</label>
+            <textarea name="message" id="message">
+              Descreva como eu posso te ajudar...
+            </textarea>
+          </form>
+        </div>
+      </div>
+    </ContactSection>
+  )
+}
+
+export default Contact
---
+++ src/components/Contact/styles.ts
@@ -0,0 +1,104 @@
+import styled from 'styled-components'
+import { colors } from '../../styles'
+
+export const ContactSection = styled.section`
+  display: flex;
+  align-items: center;
+  height: 90vh;
+
+  h2 {
+    color: ${colors.black};
+    margin-bottom: 16px;
+    font-size: 2.25rem;
+
+    span {
+      background: ${colors.gradientOne};
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;
+    }
+  }
+
+  .content {
+    display: flex;
+  }
+
+  .title {
+    text-align: center;
+    margin-bottom: 64px;
+  }
+
+  .info {
+    max-width: 50%;
+  }
+
+  h3 {
+    color: ${colors.black};
+    font-size: 1.5rem;
+    line-height: 2rem;
+    margin-bottom: 24px;
+  }
+
+  p {
+    font-size: 1.125rem;
+  }
+  .text {
+    line-height: 1.625rem;
+    font-size: 1rem;
+    margin-bottom: 32px;
+  }
+
+  svg {
+    width: 20px;
+    height: 20px;
+    color: ${colors.white};
+  }
+
+  .info-rede {
+    display: flex;
+    margin-top: 16px;
+
+    .title-contact {
+      color: ${colors.black};
+    }
+
+    .address,
+    .title-contact {
+      line-height: 1rem;
+      font-size: 0.975rem;
+      padding: 4px;
+    }
+  }
+
+  .icon {
+    background: ${colors.gradientOne};
+    width: 48px;
+    height: 48px;
+    border-radius: 8px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 12px;
+  }
+
+  form {
+    background-color: ${colors.white};
+    width: 50%;
+    border-radius: 16px;
+    border: 1px solid ${colors.border};
+    padding: 24px;
+  }
+
+  input, textarea {
+    background-color: ${colors.background2};
+    display: block;
+    margin-top: 8px;
+    padding: 8px 16px;
+    border-radius: 8px;
+    border: 1px solid ${colors.border};
+    width: 100%;
+  }
+
+  label {
+    color: ${colors.black};
+  }
+`
